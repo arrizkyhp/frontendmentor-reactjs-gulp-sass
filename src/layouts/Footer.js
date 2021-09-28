b5ddf8c5eeaadf13c987184e496268c2dd87ab2c
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-28 20:21:41 +0700

    Add reactjs src & utilities scss

---
+++ src/layouts/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+          {" "}
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/arrizkyhp" target="_blank" rel="noreferrer">
+          {" "}
+          Arrizky Hasya Pratama.{" "}
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
